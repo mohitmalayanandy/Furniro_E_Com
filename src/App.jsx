38d05c1213510110a0b380a0bcf42cb953a03b18
Mohit Malaya Nandy <mohitmalayanandy@gmail.com>
2025-03-05 16:22:14 +0530

    null

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import Home from './pages/Home/Home'
-import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
